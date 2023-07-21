1fd136fee018ff997cb7e3a25391614eed54e099
Kiran <kirandeepkaurfdk5@gmail.com>
2023-07-21 09:50:01 -0800

    data ready from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 function App() {
-  const [quote, setQuote] = useState("");
-  const getQuote = () => {
+  const [records, setRecords] = useState([]);
+  const [isError, setIsError] = useState("");
+  useEffect(() => {
     axios
-      .get("https://api.quotable.io/random")
-      .then((res) => {
-        console.log(res.data.content);
-        setQuote(res.data.content);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+      .get(
+        "https://3idb6ggz4g.execute-api.us-east-2.amazonaws.com/dev/getlast/?region=us-east-2&device_id=C4DD57B0C690"
+      )
+      .then((res) => console.log("data recieved", res))
+      .catch((err) => setIsError(err.message));
+  }, []);
   return (
     <div className="App">
-      <button onClick={getQuote}>Get Quote</button>
-      {quote ? <p>{quote}</p> : null}
+      <h1>getting axios</h1>
+      {isError !== "" && <h2>{isError}</h2>}
+      {records.map((post) => {
+        const { id, title, body } = post;
+        return (
+          <div className="card" key={id}>
+            <h2>{title.slice(0, 15).toUpperCase()}</h2>
+            <h3>{body.slice(0, 100)}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 }
